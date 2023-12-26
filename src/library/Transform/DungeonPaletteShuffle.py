276ce5342048c4286817dee7d9124e7131f15155
Christopher Wallis <christopher.j.wallis@gmail.com>
2023-12-26 10:17:37 -0700

    Palette swapping improvements

--- src/library/Transform/DungeonPaletteShuffle.py
+++ src/library/Transform/DungeonPaletteShuffle.py
@@ -2,32 +2,31 @@ from typing import Dict, List
 
 from ..Model.PaletteId import PaletteId
 
-from ..Model import DungeonRoom, DungeonRoomId, SpriteType
+from ..Model import DungeonRoom, SpriteType
 from .Context import Context
 
-_cool_dungeons = [
+_dungeon_palettes = [
+    PaletteId.x00_HYRULE_CASTLE,
+    PaletteId.x03_GREEN_DUNGEON,
     PaletteId.x04_ICE_DUNGEON,
+    PaletteId.x04_ICE_DUNGEON, # Weight Ice Dungeons higher.
+    PaletteId.x05_DESERT_DUNGEON,
     PaletteId.x06_TOWER_OF_HERA,
     PaletteId.x08_SWAMP_PALACE,
+    PaletteId.x09_DESERT_PALACE,
+    PaletteId.x09_DESERT_PALACE, # Weight Desert higher.
     PaletteId.x0A_SWAMP_PALACE,
+    PaletteId.x0B_MISERY_MIRE,
     PaletteId.x0C_AGAHNIMS_TOWER,
     PaletteId.x0D_SKULL_WOODS,
+    PaletteId.x0F_PALACE_OF_DARKNESS,
+    PaletteId.x0F_PALACE_OF_DARKNESS, # Weight POD higher.
+    PaletteId.x11_MISERY_MIRE,
+    PaletteId.x12_MISERY_MIRE_VITREUS,
     PaletteId.x13_ICE_PALACE,
     PaletteId.x14_ICE_PALACE_KHOLDSTARE,
     PaletteId.x18_TURTLE_ROCK,
     PaletteId.x19_TURTLE_ROCK_TRINEXX,
-    PaletteId.x23_BLIND,
-    PaletteId.x26_AGAHNIMS_TOWER,
-]
-_neutral_dungeons = [
-    PaletteId.x05_DESERT_DUNGEON,
-    PaletteId.x09_DESERT_PALACE,
-    PaletteId.x0B_MISERY_MIRE,
-    PaletteId.x0F_PALACE_OF_DARKNESS,
-    PaletteId.x10_PALACE_OF_DARKNESS_HELMASAUR,
-    PaletteId.x11_MISERY_MIRE,
-    PaletteId.x12_MISERY_MIRE_VITREUS,
-    PaletteId.x12_MISERY_MIRE_VITREUS,
     PaletteId.x1A_GANONS_TOWER,
     PaletteId.x1B_GANONS_TOWER,
     PaletteId.x21_GANON,
@@ -35,19 +34,6 @@ _neutral_dungeons = [
     PaletteId.x25_GANONS_TOWER,
     PaletteId.x28_GANONS_TOWER,
 ]
-_warm_dungeons = [
-    PaletteId.x00_HYRULE_CASTLE,
-    PaletteId.x03_GREEN_DUNGEON,
-    PaletteId.x05_DESERT_DUNGEON,
-    PaletteId.x09_DESERT_PALACE,
-]
-
-_palette_lists = [
-    _cool_dungeons,
-    _neutral_dungeons,
-    _warm_dungeons,
-]
-
 
 def reroll_dungeon_palette(context: Context) -> None:
     palette_dict: Dict[PaletteId, List[DungeonRoom]] = {
@@ -56,17 +42,11 @@ def reroll_dungeon_palette(context: Context) -> None:
     for room in context.dungeon_rooms.values():
         palette_dict[room.palette_id].append(room)
 
-    for palette_id, room_list in palette_dict.items():
+    for room_list in palette_dict.values():
         if len(room_list) == 0:
             continue
 
-        matching_palettes = next(
-            (it for it in _palette_lists if palette_id in it), None
-        )
-        if matching_palettes == None:
-            continue
-
-        new_palette_id = context.random.choice(matching_palettes)
+        new_palette_id = context.random.choice(_dungeon_palettes)
         for room in room_list:
             if any(
                 it
